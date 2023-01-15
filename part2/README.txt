1bdd42c0d1e5799e9cba72a789e1d6103bfbb4e9
Randy Ding <105885340+RandyD03@users.noreply.github.com>
2023-01-15 12:51:14 -0500

    Add files via upload

---
+++ part2/README.txt
@@ -0,0 +1,6 @@
+This is the solution for Part 2.
+
+Running instructions: 
+- main.py is the file with code, run this to receive output, but change the marked comment that says "#CHANGE INPUT FILE HERE" 
+- Access 2a.in (2b.in, etc.) to view the given test cases
+- Access 2a.out to receive the desired output for the test case
--- part2/main.py
+++ part2/main.py
@@ -1,1 +1,43 @@
- 
+# reading the file in
+
+# CHANGE INPUT FILE HERE
+f = open("2c.in", "r")
+kingdoms = int(f.readline())
+homes = int(f.readline())
+inputArray = f.readline().split(" ")
+misplacedArray = []
+f.close()
+
+# making an array for each home where there is a home and a number to represent its index
+# such as [['a1', 0], ['b3', 1], ['b1', 2], ['b2', 3], ['a3', 4], ['c2', 5], ['c1', 6], ['c3', 7], ['a2', 8]]
+for i in range(kingdoms * homes):
+    misplacedArray.append([inputArray[i], i])
+
+numArray = []
+
+# numArray = array with numbers representing each home
+# house represents a pair in the misplacedArray
+for home in misplacedArray:
+  numArray.append((ord(home[0][0])-97)*homes + int(home[0][1]))
+
+f = open("2c.out", "a")
+
+# our selection sort sorting algorithm
+def selectionSort(numArray, misplacedArray, size):
+    
+    for i in range(size):
+        min_index = i
+        for j in range(i + 1, size):
+            if numArray[j] < numArray[min_index]:
+                min_index = j
+              
+         # swapping the elements to sort the array
+        (numArray[i], numArray[min_index]) =(numArray[min_index],numArray[i])
+        (misplacedArray[i], misplacedArray[min_index]) = (misplacedArray[min_index],misplacedArray[i])
+      
+        # print(" swapped " + accArray[i][0] + " and " + accArray[min_index][0])
+        if misplacedArray[i][0] != misplacedArray[min_index][0]:
+          f.write("Swap " + misplacedArray[i][0] + " and " + misplacedArray[min_index][0] +"\n")
+
+selectionSort(numArray, misplacedArray, len(numArray))
+f.close()
